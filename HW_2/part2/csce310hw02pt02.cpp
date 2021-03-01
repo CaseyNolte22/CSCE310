8fbf50c624032eba269fdcf8a509fa32fc37ec01
CaseyNolte22 <casey.nolte@huskers.unl.edu>
2021-03-01 14:10:40 -0600

    HW 2 completed with extra credit

--- HW_2/part2/csce310hw02pt02.cpp
+++ HW_2/part2/csce310hw02pt02.cpp
@@ -1,9 +1,39 @@
 #include <vector>
 #include "csce310hw02pt02.h"
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int overlappingIntervals(vector<int> start , vector<int> end ){
-  return -1;
-}
+int overlappingIntervals(vector<int> start, vector<int> end)
+{
+  int maxEnd = 0;
+  int minStart = start[0];
 
+  for (int i = 0; i < end.size(); i++)
+  {
+    if (end[i] > maxEnd)
+    {
+      maxEnd = end[i];
+    }
+  }
+
+  int maxCount = 0;
+  int count = 0;
+  for (int i = minStart; i <= maxEnd; i++)
+  {
+    count = 0;
+    for (int j = 0; j < start.size(); j++)
+    {
+      if (i >= start[j] && i <= end[j])
+      {
+        count++;
+      }
+    }
+    if (count > maxCount)
+    {
+      maxCount = count;
+    }
+  }
+
+  return maxCount;
+}
--- HW_2/part3/csce310hw02pt03.cpp
+++ HW_2/part3/csce310hw02pt03.cpp
@@ -1,9 +1,24 @@
 #include "csce310hw02pt03.h"
 #include <vector>
+#include <limits>
+#include <cmath>
+#include <iostream>
 
 using namespace std;
 
-double closestNumbers( vector<double> numbers )
+double closestNumbers(vector<double> numbers)
 {
-  return 0;
+  double closest = std::numeric_limits<double>::infinity();
+
+  for (int i = 0; i < numbers.size(); i++)
+  {
+    double diff = abs(numbers[i] - numbers[i + 1]);
+    
+    if ( diff < closest)
+    {
+      closest = diff;
+    }
+  }
+
+  return closest;
 }
